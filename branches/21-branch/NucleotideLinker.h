94c56ab16f84c5f64e3df04df99f5bd5fa8a573e
mrgenixus <mrgenixus@6d041a4c-438f-4d6f-bb7b-eaab962e81b8>
2010-01-20 19:21:32 +0000

    attempting to create an unstable build branch
    git-svn-id: https://skittle.svn.sourceforge.net/svnroot/skittle@10 6d041a4c-438f-4d6f-bb7b-eaab962e81b8

---
+++ branches/21-branch/NucleotideLinker.h
@@ -0,0 +1,55 @@
+#ifndef NLINK
+#define NLINK
+
+#include <GL/glut.h>
+#include <vector>
+#include <string>
+#include <list>
+#include <map>
+#include <algorithm>
+#include <math.h>
+#include "NucleotideLink.h"
+
+#define PI 3.141592653589793
+
+using namespace std;
+
+class NucleotideLinker
+{
+public:
+	int F_width;
+	int min_width;
+	vector<NucleotideLink> links;
+
+	NucleotideLinker();
+	/*
+	inline NucleotideLink operator[] (int index)
+	{
+		return link_map[ (index/8) * 8 ];//round down index
+	}*/
+		
+	void display();
+	
+	void calculate(string sequence, int min = 200);
+	
+	vector<float> smooth( int preferred_width, int blur_size);
+	
+//	void tie_up_loose_ends(vector<float>& width_list);
+private:
+	void draw_arc(int distance);
+	
+	void DrawArc(float r, float cx = 0, float cy = 0, float start_angle = PI, float arc_angle= PI, int num_segments = 0);
+	
+	NucleotideLink multiples_check(const string& seq, NucleotideLink match, int consensus_width);
+	
+	NucleotideLink localized_search(const string& seq, int start, int dist, unsigned int range);
+	
+	void check_for_max(const string& seq, int dist, int index, short int& max, int& best_distance);
+	
+	float average( const list<int>& window );
+	
+};
+
+
+
+#endif
---
+++ branches/21-branch/TextRender.cpp
@@ -0,0 +1,58 @@
+//TextRender.cpp
+#include "TextRender.h"
+
+TextRender::TextRender()
+{
+}
+
+
+void TextRender::range_labels(int start, int stop, point startX, point stopX, int granularity)
+{//GLUT_BITMAP_8_BY_13;
+/*GLUT_BITMAP_8_BY_13
+GLUT_BITMAP_9_BY_15
+GLUT_BITMAP_TIMES_ROMAN_10
+GLUT_BITMAP_TIMES_ROMAN_24
+GLUT_BITMAP_HELVETICA_10
+GLUT_BITMAP_HELVETICA_12
+GLUT_BITMAP_HELVETICA_18*/
+
+	int font=(int)GLUT_BITMAP_HELVETICA_18;
+	if(granularity < 1) 
+		granularity = 1;
+	double step =   536870912;//a very large power of 2
+	while(step/2 > granularity)
+		step = step / 2;
+	//double Xstep = ((double)(stopX-startX)) / (stop-start) * step;
+	//double x = startX;
+	
+	for(double i = start; i < stop; i += step)
+	{		
+		stringstream oss;//generate label
+		oss << (int)i;	
+		point p1 = startX.interpolate(stopX, (i-start)/(stop-start));
+		renderBitmapCharacher(p1.x, p1.y, p1.z, (void *)font, oss.str().c_str());
+		//pointer triangle
+		glPushMatrix();
+			glTranslated(p1.x, p1.y -1, p1.z);
+			glBegin(GL_TRIANGLES);
+		        glVertex3d(.5, 0, 0);
+		        glVertex3d(-.5, 0, 0);
+		        glVertex3d(0, -1, 0);
+		    glEnd();
+		glPopMatrix();
+	}	
+}
+
+void TextRender::renderBitmapCharacher(float x, float y, float z, void *font, const char *str)
+{
+	const char *c;
+	//glColor3f(0,0,0);
+	glRasterPos3f(x, y, z);
+	for (c=str; *c != '\0'; c++) 
+	{
+		glutBitmapCharacter(font, *c);
+	}
+}
+
+
+
