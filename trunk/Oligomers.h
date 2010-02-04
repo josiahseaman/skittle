73e5f8f7cc9e7bc097eabd372df2f281382711c0
mrgenixus <mrgenixus@6d041a4c-438f-4d6f-bb7b-eaab962e81b8>
2010-02-04 20:05:30 +0000

    Added files
    git-svn-id: https://skittle.svn.sourceforge.net/svnroot/skittle@23 6d041a4c-438f-4d6f-bb7b-eaab962e81b8

---
+++ trunk/Oligomers.h
@@ -0,0 +1,74 @@
+#ifndef OLIG_DISP
+#define OLIG_DISP
+
+#include <GL/glut.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include <QGLWidget>
+#include "BasicTypes.h"
+#include "FrequencyMap.h"
+//#include "AbstractGraph.h"
+#include "SkittleUi.h"
+#include "TextureCanvas.h"
+
+using namespace std;
+
+class Oligomers : public FrequencyMap //AbstractGraph//
+{
+    Q_OBJECT
+    
+public:
+	
+	bool texture_optimization;
+	
+	Oligomers(Ui_SkittleGUI* gui);
+	~Oligomers();
+	void createConnections();
+	void checkVariables();
+	void display();
+	void load_canvas();
+	GLuint render();
+	void freq_map();
+	int check_height();
+	void mouseClick(point2D pt);
+	
+	void display_freq();
+	void calculate(vector<color>& img, int vote_size);
+	vector<color> calculateBoundaries(vector<color>& img, int row_size, int graphWidth);
+	double correlate(vector<color>& img, int beginA, int beginB, int pixelsPerSample);
+	
+	inline int width() const
+	{
+		int widthMultiplier = 1;
+		if(wordLength < 3)	{		widthMultiplier = 4;	}
+		else if(wordLength == 3)	{		widthMultiplier = 2;	}
+	
+		return F_width*widthMultiplier + 50;
+	}
+	
+	inline int ACGT_num(char n)
+	{
+		if(n == 'A') return 0;
+		if(n == 'T') return 3;
+		if(n == 'C') return 1;
+		if(n == 'G') return 2;
+	
+		return -100000;//handles up to 8-mer correctly
+	}
+	
+public slots:	
+	void changeWordLength(int);
+	void toggleVisibility();
+signals:
+	void wordLengthChanged(int);
+	
+private:
+	TextureCanvas* graphBuffer;
+	int wordLength;
+
+};
+
+#endif
