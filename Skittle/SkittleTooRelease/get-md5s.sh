88ff9870fa98325f6d5928d3c0b4d9e838f59003
Marshall Smith <marshall.ds@gmail.com>
2012-09-22 14:15:32 -0600

    fix script to ignore itself and its output file

--- Skittle/SkittleTooRelease/get-md5s.sh
+++ Skittle/SkittleTooRelease/get-md5s.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+#requires md5sum
 
-TARGET="*.*" # relative path to target files
+TARGET="htdocs/*.*" # relative path to target files
 
 touch files.list #create file in case it doesn't exist
 cat /dev/null > files.list #clear file in case you've already done this before
 
 for file in $TARGET
 do
-	md5sum "$file" >> files.list
+	if [ $file != "files.list" ] && [ $file != "get-md5s.sh" ]; then
+		md5sum "$file" >> files.list
+	fi
 done
