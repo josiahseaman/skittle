61521c06695b1c2cf1748d1e35aa0689adb3877b
Marshall Smith <marshall.ds@gmail.com>
2012-09-24 10:13:10 -0600

    echo message to terminal when run

--- Skittle/SkittleTooRelease/get-md5s.sh
+++ Skittle/SkittleTooRelease/get-md5s.sh
@@ -6,9 +6,14 @@ TARGET="*.*" # relative path to target files
 touch files.list #create file in case it doesn't exist
 cat /dev/null > files.list #clear file in case you've already done this before
 
+FCOUNT=0;
+
 for file in $TARGET
 do
 	if [ $file != "files.list" ] && [ $file != "get-md5s.sh" ]; then
 		md5sum "$file" >> files.list
+		FCOUNT=$((FCOUNT+1))
 	fi
 done
+
+echo "MD5 Hashes saved for $FCOUNT files to files.list"
