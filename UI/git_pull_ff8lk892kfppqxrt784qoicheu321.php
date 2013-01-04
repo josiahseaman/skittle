02de468372f76efa2377fbc53c914be770fd3b68
Bryan <bryan@newlinetechnicalinnovations.com>
2013-01-04 15:11:00 -0700

    Start server configuration

---
+++ UI/git_pull_ff8lk892kfppqxrt784qoicheu321.php
@@ -0,0 +1,16 @@
+<?php
+try
+{
+    $payload = json_decode($_REQUEST['payload']);
+}
+catch(Exception $e)
+{
+    exit(0);
+}
+
+file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+
+if($payload->ref === 'refs/heads/master')
+{
+    exec('../git_pull_ff8lk892kfppqxrt784qoicheu321.sh');
+}
