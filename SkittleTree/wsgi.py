2c7771a51f2479ed0ad0338c76a2d9459fd13c72
Bryan <bryan@newlinetechnicalinnovations.com>
2013-01-05 13:13:39 -0700

    Add project to system path

--- SkittleTree/wsgi.py
+++ SkittleTree/wsgi.py
@@ -13,7 +13,9 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os, sys
+
+sys.path.append('/var/www/skittle')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SkittleTree.settings")
 
