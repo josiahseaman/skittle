ea426628f2a87bc930645b546408658eef0bb1cd
Marshall Smith <marshall.ds@gmail.com>
2013-01-05 14:26:05 -0700

    change view to use render so the static url will be passed

--- SkittleCore/views.py
+++ SkittleCore/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,render
 from django.conf import settings 
 from django.http import HttpResponseRedirect, HttpResponse
 
 def index(request):
-    return render_to_response('index.html')
+    # return render_to_response('index.html')
+    return render(request, 'index.html')
