37b3f823782bdebd78978f53ccc01b8fcae24771
Bryan <bryan@newlinetechnicalinnovations.com>
2013-01-05 21:48:45 -0700

    Test hostname check

--- SkittleTree/wsgi.py
+++ SkittleTree/wsgi.py
@@ -16,8 +16,11 @@ framework.
 import os, sys, socket
 
 if socket.gethostname().startswith('nyx'):
+    logging.debug("IM ON NYX!!!")
     sys.path.append('/var/www/skittle')
-
+else:
+    logging.debug("IM MESSED UP!!!")
+    
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SkittleTree.settings")
 
 # This application object is used by any WSGI server configured to use this
