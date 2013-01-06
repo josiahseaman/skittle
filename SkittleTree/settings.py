1dd33cf6b07edd194eb9ac185031009ae270ce67
Bryan <bryan@newlinetechnicalinnovations.com>
2013-01-05 20:27:24 -0700

    Test caller id function

--- SkittleTree/settings.py
+++ SkittleTree/settings.py
@@ -6,7 +6,11 @@ TEMPLATE_DEBUG = DEBUG
 
 if socket.gethostname().startswith('nyx'):
     caller = inspect.stack()[1][1]
-
+    try:
+        raise NameError(inspect.stack())
+    except NameError:
+        print 'Stop!'
+        raise
     if "importlib.py" in caller:
         SkittleTreeLoc = os.getcwd() + "/"
     else:
