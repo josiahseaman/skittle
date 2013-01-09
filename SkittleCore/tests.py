e42c1bdc3e2fab666b72f65108132c5233c1f908
Bryan <bryan@newlinetechnicalinnovations.com>
2013-01-09 16:07:02 -0700

    Create basic server launch tests

--- SkittleCore/tests.py
+++ SkittleCore/tests.py
@@ -1,16 +1,10 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestServerLaunch(TestCase):
+    def test_index(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        
+    def test_browse(self):
+        resp = self.client.get('/browse/')
+        self.assertEqual(resp.status_code, 200)
