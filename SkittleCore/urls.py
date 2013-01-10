2804720537987cd3610339a5cbe32d22f875e51b
Marshall Smith <marshall.ds@gmail.com>
2013-01-10 15:19:16 -0700

    graph url parsing

--- SkittleCore/urls.py
+++ SkittleCore/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
 
 from SkittleCore import views
+import SkittleRequestHandler
 
 urlpatterns = patterns('',
     url(r'^(?P<fasta>\w+)/$', views.index, name='index'),
+    url(r'^n-display.png$', views.graph, name='graph'),
+    url(r'^(?P<chromosome>\w+)_(?P<graph>\w)_(?P<start>\d+)_(?P<width>\d+)_(?P<scale>\d+)\.png$', views.graph, name='graph'),
     url(r'^$', views.index, name='index'),
 )
--- SkittleCore/views.py
+++ SkittleCore/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render_to_response,render
 from django.conf import settings 
 from django.http import HttpResponseRedirect, HttpResponse, HttpRequest, QueryDict
+from SkittleCore.SkittleRequestHandler import handleRequest
+from SkittleCore.models import StatePacket
 
 def index(request, fasta="test"):
-    # return render_to_response('index.html')
     width = request.GET.get('width',100)
     scale = request.GET.get('scale',1)
     start = request.GET.get('start',1)
@@ -11,3 +12,15 @@ def index(request, fasta="test"):
     graphs = request.GET.get('graphs',"n")
     context = {'fasta':fasta,'width':width, "scale":scale,"start":start,"zoom":zoom,"graphs":graphs}
     return render(request, 'index.html',context)
+
+def graph(request,chromosome="",graph="n",start=1,width=1,scale=1):
+	state = StatePacket()
+	state.chromosome = chromosome
+	state.graph = graph
+	state.start = start
+	state.width = width
+	state.scale = scale
+	state.requestedGraph = graph
+	image_data = handleRequest(state)
+	# image_data = open("/Users/marshallds/Sites/Skittle/master/SkittleCore/UI/assets/n-display.png", "rb").read()
+	return HttpResponse(image_data, mimetype="image/png")
