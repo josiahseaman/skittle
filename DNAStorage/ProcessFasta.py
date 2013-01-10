d41e3ac942f606ff84dd6aa7ec10c84ad96b8612
Bryan <bryan@newlinetechnicalinnovations.com>
2013-01-10 16:05:23 -0700

    Fasta Processor
    This is the initial version of Fasta Processor.
    It takes fasta or fa files from the Curator person, splits them, strips
    them, and sorts them.
    It is a little slow now as a better way for skipping the first line
    needs to be made.

--- DNAStorage/ProcessFasta.py
+++ DNAStorage/ProcessFasta.py
@@ -1,14 +1,13 @@
 import sys, os, os.path, re
 
 #Take a fasta file, split it and sort it into the correct folders
-def splitAndSort(file, storageLocation):
+def splitAndSort(file, storageLocation, workingLocation):
     bp = 65536
     
     #Take the file name and split it at each delim.
     #Then check to make sure that we have all 6 identifiers:
     #Kingdom, Class, Genus, Species, Specimen, Chromosome
-    #TODO: Convert this regex to be one line instead of two
-    fileName = re.sub('\.fa[sta]', '', file)
+    fileName = re.sub('\.fa?[sta]', '', file)
     
     #Parse file name into system path
     taxonomic = fileName.split("_")
@@ -23,7 +22,35 @@ def splitAndSort(file, storageLocation):
         os.makedirs(filePath)
         
     #Split the file every $bp
-    
+    with open(workingLocation + file) as f:
+        fCount = 1
+        cCount = 0
+        chunk = ""
+        skip = False
+        while True:
+            character = f.read(1)
+            if character:
+                if character == ">":
+                    skip = True
+                if skip:
+                    if character == "\n":
+                        skip = False
+                else:
+                    if character != "\n":
+                        chunk += character
+                        cCount += 1
+                        if cCount == bp:
+                            writePath = os.path.join(filePath, str(fCount) + "-" + str((fCount + cCount - 1)) + ".fasta")
+                            write = open(writePath, 'w')
+                            write.write(chunk.upper())
+                            chunk = ""
+                            fCount += cCount
+                            cCount = 0
+            else:
+                break;
+        writePath = os.path.join(filePath, str(fCount) + "-" + str((fCount + cCount - 1)) + ".fasta")
+        write = open(writePath, 'w')
+        write.write(chunk)
 
 #----------------------------------------------------------------------------------------
 #CD into the folder where this file is located as it should be the DNAStorage folder
@@ -36,8 +63,8 @@ os.chdir(workingDir)
 for file in os.listdir("./to_import/"):
     if file.endswith(".fasta") or file.endswith(".fa"):
         try:
-            splitAndSort(file, workingDir + "/fasta")
-            os.rename("to_import/" + file, "history/" + file)
+            splitAndSort(file, workingDir + "/fasta", workingDir + "/to_import/")
+            #os.rename("to_import/" + file, "history/" + file)
         except IOError as ex:
             print ex
             os.rename("to_import/" + file, "rejected/" + file)
