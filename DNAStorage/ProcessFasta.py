44c2c4c1d23d3152af2dde8588100ab96b8f717a
Marshall Smith <marshall.ds@gmail.com>
2013-01-11 15:44:34 -0700

    fix processfasta to work on UNIX

--- DNAStorage/ProcessFasta.py
+++ DNAStorage/ProcessFasta.py
@@ -23,9 +23,9 @@ def splitAndSort(file, storageLocation, workingLocation):
         os.makedirs(filePath.replace("fasta", "png"))
     
     #Remove first line if needed and depending on OS
-    if os.name == "posix" or os.name == "mac":
+    if os.name == "posix":
         #FAST SED COMMAND ON LINUX
-        bashCommand = "sed -i '1{/>/d;}'" + (workingLocation + file)
+        bashCommand = "sed -i '' '1{/>/d;}' " + (workingLocation + file)
         os.system(bashCommand)
     else: #SLOW WINDOWS VERSION
         skip = False;
