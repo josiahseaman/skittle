ce4f636f9352ab816b983bdfd6f7fc3480208095
Bryan <bryan@newlinetechnicalinnovations.com>
2013-01-17 16:13:23 -0700

    Remove last concession

--- DNAStorage/StorageRequestHandler.py
+++ DNAStorage/StorageRequestHandler.py
@@ -8,8 +8,8 @@ def HasFastaFile(specimen, chromosome):
     
 #Searches to see if the given fasta file is stored in the system. If so, it returns the system path to the requested chunk
 def GetFastaFilePath(specimen, chromosome, start):
-    fastaFile = FastaChunkFiles.objects.filter(FastaFile__Specimen = str(specimen), FastaFile__Chromosome = str(chromosome), Start = start)[:1]
-    
+    fastaFile = FastaChunkFiles.objects.filter(FastaFile__Specimen = specimen, FastaFile__Chromosome = chromosome, Start = start)[:1]
+    print specimen + chromosome + str(start)
     if fastaFile:
         #Check if fasta file is stored in ram disk
         if fastaFile[0].IsInRamDisk:
