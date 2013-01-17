d903ba92b9c8f33d2ccf4b5548c41ae3d76b5a07
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-01-17 13:07:38 -0700

    Refactored ServerGraphDescription

--- SkittleCore/GraphRequestHandler.py
+++ SkittleCore/GraphRequestHandler.py
@@ -27,6 +27,7 @@ import Graphs.SequenceHighlighter
 import Graphs.SimilarityHeatMap
 import Graphs.ThreeMerDetector
 from Graphs.SkittleGraphTransforms import hasDepth
+from PngConversionHelper import convertToPng
 '''Finally, X = __import__('X') works like import X, with the difference that you 
 1) pass the module name as a string, and 2) explicitly assign it to a variable in your current namespace.'''
 
@@ -51,64 +52,6 @@ def calculatePixels(state):
         results = graphModule.calculateOutputPixels(state)
     return results
 
-def checkForGreyscale(state):
-    grayGraph = ['m', 'o', 's']
-    return state.requestedGraph in grayGraph
-
-def convertToPng(state, pixels):
-    targetWidth = 1024
-    greyscale = checkForGreyscale(state)
-    print "GreyScale: ", greyscale
-    f = open(state.getPngFilePath(), 'wb')
-    if greyscale:
-        p = multiplyGreyscale(pixels, 255)
-        w = png.Writer(len(p[0]), len(p), greyscale=True)
-    elif not isRasterGraph(state):   #Nucleotide Bias
-        p = flattenImage(pixels, len(pixels[0]), True, 4)
-        w = png.Writer(len(p[0])/4, len(p), greyscale=False, alpha=True)
-    else: #raster, color graphs
-        p = flattenImage(pixels, targetWidth)
-        w = png.Writer(targetWidth, 64)
-    w.write(f, p)
-    f.close()
-    data = open(state.getPngFilePath(), 'rb').read() #return the binary contents of the file
-    return data
-
-def flattenImage(pixels, targetWidth, isColored = True, nChannels = 3, depth = 0):
-#    if isinstance(pixels, list) and len(pixels) != 0:
-##        return map(lambda x: flattenImage(x, targetWidth, isColored, nChannels, depth+1), pixels) 
-#        if isinstance(pixels[:1], list):#TODO: something about hasDepth
-#            return reduce(lambda x,y: x + flattenImage(y, targetWidth, isColored, nChannels), pixels, [])
-#        else:
-#            return pixels
-#    else:
-#        return pixels
-    pixels = squishImage(pixels)
-    
-    p = []
-    newline = []
-    for color in pixels:
-        if color is None:
-            newline += (0,) * nChannels
-        elif nChannels == 4:
-            newline += color 
-            newline += (255,) #alpha
-        else: 
-            newline += color
-        if len(newline) >= targetWidth * nChannels: 
-            p.append(newline)
-            newline = []
-    return p
-
-def squishImage(pixels):
-    if isinstance(pixels, list):
-        if isinstance(pixels[0], list):
-            return reduce(lambda x,y: x + squishImage(y), pixels, [])
-        else:
-            return pixels
-    else:
-        return pixels
-
 '''The main entry point for the whole Python logic SkittleCore module and Graphs.'''
 def handleRequest(state):
     assert isinstance(state, StatePacket)
@@ -121,17 +64,13 @@ def handleRequest(state):
     if png is None:
         pixels = calculatePixels(state)
 #        print pixels[:10]
-        png = convertToPng(state, pixels)
+        png = convertToPng(state, pixels, isRasterGraph(state))
     return png
 
 def isRasterGraph(state):
     graphDescription = getGraphDescription(state)
     return graphDescription[3]
 
-def multiplyGreyscale(p, greyMax = 255):
-    for index, line in enumerate(p):
-        p[index] = map(lambda x: int(max(x,0.0) * greyMax), line)
-    return p
     
 def getGraphDescription(state):
     targetGraphTuple = filter(lambda x: state.requestedGraph == x[0], availableGraphs)
@@ -149,11 +88,15 @@ def tryGetGraphPNG(state):
     except:
         return None
     
+class ServerSideGraphDescription():
+    def __init__(self, Name, IsRaster):
+        self.name = Name
+        self.rasterGraph = IsRaster    
+    
 def generateGraphListForServer():
     graphs = {}
-    ServerDescription = namedtuple('ServerGraphDescription', ['name', 'rasterGraph'])
     for description in availableGraphs:
-        graphs[description[0]] = ServerDescription(description[1], description[3])
+        graphs[description[0]] = ServerSideGraphDescription(description[1], description[3])
     return graphs
         
         
