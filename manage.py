72b669a89911272fc3db6c4fc2536e0ac7d555dc
Bryan <bryan@newlinetechnicalinnovations.com>
2013-01-21 18:27:11 -0700

    Change admin command architecture. To import fasta files, now use 'manage.py importfasta'. To delete the cache of a certain type of graph, you 'will' use 'manage.py deletecache x'. Also fixed runServer to not prompt when migration is needed.

--- manage.py
+++ manage.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-
-def importFasta():
-    from DNAStorage.ProcessFasta    import run
-    run()
     
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SkittleTree.settings")
 
     from django.core.management import execute_from_command_line
-
-    if "ImportFasta" in sys.argv or "importFasta" in sys.argv:
-        importFasta()
-    else:
-        execute_from_command_line(sys.argv)
+    
+    execute_from_command_line(sys.argv)
--- startDevServer.sh
+++ startDevServer.sh
@@ -2,6 +2,6 @@
 
 {
 python ./manage.py collectstatic -v0 --noinput
-python ./manage.py evolve --hint --execute
+python ./manage.py evolve --hint --execute --noinput
 python ./manage.py runserver 5000
 }
