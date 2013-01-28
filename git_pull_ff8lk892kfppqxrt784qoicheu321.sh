a90a60cdb102ef33d17e64fbab02345fa1980393
Bryan <bryan@newlinetechnicalinnovations.com>
2013-01-28 13:14:40 -0700

    Server auto pull should also auto migrate

--- git_pull_ff8lk892kfppqxrt784qoicheu321.sh
+++ git_pull_ff8lk892kfppqxrt784qoicheu321.sh
@@ -3,6 +3,6 @@
 cd /var/www/skittle
 git pull
 python ./manage.py collectstatic -v0 --noinput
-python ./manage.py evolve --hint --execute
+python ./manage.py evolve --hint --execute --noinput
 touch SkittleTree/wsgi.py
 }
