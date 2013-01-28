8a4fdbd4cb9baa92f37cc963dc399b5ff869fd01
Bryan <bryan@newlinetechnicalinnovations.com>
2013-01-28 13:56:38 -0700

    Rework storage models to store chromosomes under Specimen

--- DNAStorage/models.py
+++ DNAStorage/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 
-class FastaFiles(models.Model):
-    Specimen = models.CharField(max_length=50)
-    Chromosome = models.CharField(max_length=50)
+class Specimen(models.Model):
     Species = models.CharField(max_length=50)
     Genus = models.CharField(max_length=50)
     Class = models.CharField(max_length=50)
     Kingdom = models.CharField(max_length=50)
-    Length = models.IntegerField()
+    GenomeLength = models.IntegerField()
     Source = models.CharField(max_length=255, null=True)
     ExtendedName = models.CharField(max_length=255, null=True)
     Description = models.TextField(null=True)
+
+class Chromosome(models.Model):
+    Specimen = models.ForeignKey(Specimen)
+    Chromosome = models.CharField(max_length=50)
+    Length = models.IntegerField()
     
-class FastaChunkFiles(models.Model):
-    FastaFile = models.ForeignKey(FastaFiles)
+class ChromosomeChunkFiles(models.Model):
+    Chromosome = models.ForeignKey(Chromosome)
     Start = models.IntegerField()
     IsInRamDisk = models.BooleanField(default=False)
 
 class ImageFiles(models.Model):
-    FastaFile = models.ForeignKey(FastaFiles)
+    Chromosome = models.ForeignKey(Chromosome)
     GraphType = models.CharField(max_length=1)
     Start = models.IntegerField()
     Scale = models.IntegerField(null=True)
