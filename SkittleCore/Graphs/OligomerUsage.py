6ed260c0ab3f37b8e199f3128f12cc41712741e7
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-01-30 15:21:24 -0700

    Oligomer Usage: Fixed normalization in unsequenced areas

--- SkittleCore/Graphs/OligomerUsage.py
+++ SkittleCore/Graphs/OligomerUsage.py
@@ -20,13 +20,16 @@ def calculateOutputPixels(state, oligState = OligomerUsageState()):
 #    print lines
     counts = countNucleotides(lines, oligState.oligomerSize)
     
-    values = map(lambda x: x.values(), counts)
-    single = reduce(lambda current, ilist: current+ilist, values, [])
-    wholeScreenMaximum = max(single)
-    
-    counts = normalizeDictionary(counts, wholeScreenMaximum)#this is currently per line normalization.  Test to see which is more/less confusing
-#    print counts
-    #per line normalization is going to screw up the math used in the similarity heat map, make sure not to use normalized data for that
+    #NORMALIZATION
+    values = []
+    for line in counts:
+        for key, value in line.iteritems():
+            if key[0] != 'N':
+                values.append(value)
+
+    wholeScreenMaximum = max(values)
+    counts = normalizeDictionary(counts, wholeScreenMaximum)
+
     #TODO: create a sparse display for the oligomer display
     orderedWords = generateExhaustiveOligomerList(oligState.oligomerSize)
     pixels = oligCountToColorSpace(counts, orderedWords)
