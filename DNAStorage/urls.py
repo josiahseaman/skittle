724978e397bcf3c5399ecbd41adda555671568e3
Marshall Smith <marshall.ds@gmail.com>
2013-02-22 09:02:33 -0700

    fasta upload interface + taxonomy for autofill

--- DNAStorage/urls.py
+++ DNAStorage/urls.py
@@ -4,4 +4,6 @@ from DNAStorage import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^upload$', views.upload, name='upload'),
+    url(r'^taxonomy.json$', views.taxonomy, name='taxonomy'),
 )
--- DNAStorage/views.py
+++ DNAStorage/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render_to_response,render
 from django.conf import settings 
 from django.http import HttpResponseRedirect, HttpResponse, HttpRequest, QueryDict
+from django.utils import simplejson
 from DNAStorage.models import Specimen,FastaFiles
+from DNAStorage import StorageRequestHandler
 
 
 def index(request):
@@ -10,3 +12,10 @@ def index(request):
     context = {'specimens':specimens,'chromosomes':chromosomes}
     return render(request, 'index.html',context)
 
+def upload(request):
+	context = {}
+	return render(request, 'upload.html', context)
+
+def taxonomy(request):
+    json = "currentTaxonomy = " + simplejson.dumps(StorageRequestHandler.GetTreeList())
+    return HttpResponse(json,content_type="application/json")
