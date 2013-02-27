0a0d445fbc2df56efa359f74e65716c84951f54d
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-02-26 18:18:40 -0700

    Moved django requests inside of parallel section

--- precompute.py
+++ precompute.py
@@ -1,21 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import os
 import sys
+from collections import namedtuple
 from multiprocessing import Pool
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SkittleTree.settings")
-from SkittleCore import GraphRequestHandler
-from DNAStorage import StorageRequestHandler
-from SkittleCore import models  
-from SkittleCore.Graphs import RepeatMap
 
 
-def computeRequest(state):
-    print "Computing: ", state.specimen, state.chromosome, state.start    
-    GraphRequestHandler.handleRequest(state)
-    print "Done computing ", state.specimen, state.chromosome, state.start
+def computeRequest(request):
+    
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SkittleTree.settings")
+    from SkittleCore import GraphRequestHandler
+    from DNAStorage import StorageRequestHandler
+    
+    specimen = request[0]
+    chromosomes = StorageRequestHandler.GetRelatedChromosomes(specimen)
+    for chromosome in chromosomes:
+        length = StorageRequestHandler.GetChromosomeLength(specimen, chromosome)
+        chunks = range(1, length+1, 2**16)
+        for targetIndex in range(request[2], len(chunks), request[1]):
+            start = chunks[targetIndex]
+            state = makePacket(specimen, chromosome, start)
+            
+            print "Computing: ", state.specimen, state.chromosome, state.start    
+            GraphRequestHandler.handleRequest(state)
+            print "Done computing ", state.specimen, state.chromosome, state.start
 
 def makePacket(specimen, chromosome, start):
+    from SkittleCore import models  
+    from SkittleCore.Graphs import RepeatMap
+    
     state = models.RequestPacket()
     state.specimen = specimen
     state.chromosome = chromosome
@@ -27,12 +40,10 @@ def makePacket(specimen, chromosome, start):
 
 if __name__ == "__main__":
         
+    nProcessors = 6
     specimen = sys.argv[1]
-    chromosomes = StorageRequestHandler.GetRelatedChromosomes(specimen)
-    for chromosome in chromosomes:
-        length = StorageRequestHandler.GetChromosomeLength(specimen, chromosome)
-        chunks = range(1, length+1, models.chunkSize)
-        requests = map(lambda start: makePacket(specimen, chromosome, start), chunks)
-        processors = Pool(6)
-        
-        processors.map(computeRequest, requests)
+#    ProcessorRequest = namedtuple('ProcessorRequest', ['specimen', 'nProcessors', 'PID'])
+    requests = [(specimen, nProcessors, PID) for PID in range(nProcessors)]
+    processors = Pool(nProcessors)
+    processors.map(computeRequest, requests)
+    
