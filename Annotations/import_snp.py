a79807ac88a01ef313f2f8485f15be1abf744a86
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-03-21 15:01:47 -0600

    createSnpIndex now prints out percentage completion

--- Annotations/import_snp.py
+++ Annotations/import_snp.py
@@ -10,10 +10,15 @@ from django.conf import settings
 
 def createSnpIndex():
     indexFile = open(settings.SKITTLE_TREE_LOC+'Annotations/snps.index.sorted.txt', 'r')
-    for line in indexFile:
+    index = 0
+    for line  in indexFile:
         tokens = line.split()
+        if len(tokens) < 4: continue
         entry, created = SnpIndexInfo.objects.get_or_create(Start=tokens[0], SnpName=tokens[1], Chromosome=tokens[2], CompactIndex=tokens[3])
 #        print entry
+        if index % 1000 == 0:
+            print index / 1047958.0 * 100
+        index += 1
         
 def createAnnotationsFromCompact(clientName, chromosome, start):
     chunkSnps = {'SNP_' + clientName: dict()}
