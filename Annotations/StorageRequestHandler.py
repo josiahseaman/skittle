ba71dc2b9463a0b0349993fec5c7fd4bd58d2ff9
Bryan <bryan@newlinetechnicalinnovations.com>
2013-03-21 11:39:15 -0600

    Rework GetAnnotationsChunk to pull All or just specific gff files related to the request instead of only the first one.

--- Annotations/StorageRequestHandler.py
+++ Annotations/StorageRequestHandler.py
@@ -24,24 +24,37 @@ def StoreAnnotationChunk(gff, chromosome, chunk, start):
        
     chunkDB, created = AnnotationJsonChunk.objects.get_or_create(GFF = gff, Chromosome = chromosome, Start = start, IsInRamDisk = False)
     
-def GetAnnotationsChunk(specimen, chromosome, start):
-    annotationJsonChunk = AnnotationJsonChunk.objects.filter(GFF__Specimen__Name = specimen, Chromosome = chromosome, Start = start)[:1]
+def GetAnnotationsChunk(specimen, chromosome, start, annotations = None):
+    annotationJsonChunk = list()
+    if annotations:
+        #Go through each given gff file
+        for gff in annotations:
+            temp = AnnotationJsonChunk.objects.filter(GFF__Specimen__Name = specimen, Chromosome = chromosome, Start = start, GFF__FileName = gff)[:1]
+            if temp:
+                annotationJsonChunk.append(temp[0])
+    else:
+        #Grab all gff files
+        temp = AnnotationJsonChunk.objects.filter(GFF__Specimen__Name = specimen, Chromosome = chromosome, Start = start)
+        if temp:
+            for annotation in temp:
+                annotationJsonChunk.append(annotation)
     
-    if annotationJsonChunk:
-        annotationJsonChunk = annotationJsonChunk[0]
+    if len(annotationJsonChunk) >= 1:
+        contents = ""
+        for annotation in annotationJsonChunk:
+            gff = annotation.GFF
+            fastaFile = GetRelatedFastaFile(gff.Specimen, chromosome)
+            
+            annotationChunkFilePath = settings.SKITTLE_TREE_LOC + "Annotations/chunks/" + fastaFile.Specimen.Kingdom + "/" + fastaFile.Specimen.Class + "/" + fastaFile.Specimen.Genus + "/" + fastaFile.Specimen.Species + "/" + fastaFile.Specimen.Name + "/" + fastaFile.Chromosome + "/" + generateAnnotationChunkName(gff, start)
+            chunkFile = open(annotationChunkFilePath, 'r')
+            
+            read = chunkFile.read()
+            chunkFile.close()
+            read = read[:-1] + ","
+            
+            contents = contents + read
+        
+        contents = contents[:-1] + "}"
+        return contents
     else:
         return None
-    
-    gff = annotationJsonChunk.GFF
-    
-    fastaFile = GetRelatedFastaFile(gff.Specimen, chromosome)
-    
-    annotationChunkFilePath = settings.SKITTLE_TREE_LOC + "Annotations/chunks/" + fastaFile.Specimen.Kingdom + "/" + fastaFile.Specimen.Class + "/" + fastaFile.Specimen.Genus + "/" + fastaFile.Specimen.Species + "/" + fastaFile.Specimen.Name + "/" + fastaFile.Chromosome + "/" + generateAnnotationChunkName(gff, start)
-    
-    chunkFile = open(annotationChunkFilePath, 'r')
-    
-    contents = chunkFile.read()
-    
-    chunkFile.close()
-    
-    return contents
