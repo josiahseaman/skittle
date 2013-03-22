ddf8306b4511553ea8fbd6aa7509f0bc7e7d69c3
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-03-22 11:30:27 -0600

    django syntax

--- Annotations/import_snp.py
+++ Annotations/import_snp.py
@@ -27,11 +27,7 @@ def createAnnotationsFromCompact(clientName, chromosome, start):
 #    f.close()
     compactString =SkittleCore.Graphs.SNPdata.packedSNPs 
     
-    #array with 8 values, coming from the GFF file.  Reorder slightly
-    
-    
-    
-    for snp in SnpIndexInfo.objects.filter(Chromosome=chromosome, Start_ge=start, Start_lt=start+settings.CHUNK_SIZE):
+    for snp in SnpIndexInfo.objects.filter(Chromosome=chromosome, Start__gte=start, Start__lt=start+settings.CHUNK_SIZE):
         uniqueID = snp.SnpName
         chunkSnps['SNP_' + clientName][uniqueID] = {"Start": snp.start, "Allele 1":compactString[snp.CompactIndex*2], "Allele 2":compactString[snp.CompactIndex*2+1]}
     assert len(snp) != 0, "SNP index not loaded"
