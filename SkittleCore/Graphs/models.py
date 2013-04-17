bf5687e5504c2a91f3cd5319a707ce79ad58a474
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-04-17 10:44:24 -0600

    de-django the state models

--- SkittleCore/Graphs/models.py
+++ SkittleCore/Graphs/models.py
@@ -9,7 +9,7 @@ class ParentState():
         abstract = True
     
 class AnnotationDisplayState(ParentState):
-    annotationPath = models.URLField()
+    annotationPath = ''
     
 class HighlighterState(ParentState):
     searchReverseComplement = True
@@ -32,8 +32,8 @@ This is the link and definition of the settings tab for Repeat Map.
 I'm dubious about where to put this since GraphTransforms depends on it, but otherwise
 it would be included in RepeatMap.py'''
 class RepeatMapState(ParentState):
-    F_width = models.IntegerField(default=11)
-    F_start = models.IntegerField(default=1)
+    F_width = 11
+    F_start = 1
     skixelsPerSample = 24
     
     def height(self, state, pixels):
@@ -56,14 +56,14 @@ class NucleotideBiasState(ParentState):
     pass
     
 class OligomerUsageState(ParentState):
-    oligomerSize = models.IntegerField(default=2)
+    oligomerSize = 2
     
 class SimilarityHeatMapState(OligomerUsageState):
     useRowColumnCorrelation = False
 
 class ThreeMerDetectorState(ParentState):
-    barWidth = models.IntegerField(default=40)    #used for display size calculations
-    samples = models.IntegerField(default=20)
+    barWidth = 40    #used for display size calculations
+    samples = 20
     
     def height(self, state, seq):
         F_height = int(math.ceil( ((len(seq)) - (self.samples*3)*state.scale ) / state.nucleotidesPerLine() ))
