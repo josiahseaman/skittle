e761ec3ac910413cc6609e7d5cc39731b06e5625
Bryan <bryan@newlinetechnicalinnovations.com>
2013-05-16 13:38:18 -0600

    Linux test of setting different user privileges on script

--- manage.py
+++ manage.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
+import socket
+
+if socket.gethostname().startswith('nyx') or socket.gethostname().startswith('skittle'):
+    os.setuid(33)
+    import getpass
+    print getpass.getuser()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SkittleTree.settings")
