96ee3ec0af435ab409bcb0473eeded2ad236c553
Bryan <bryan@newlinetechnicalinnovations.com>
2013-05-23 10:34:16 -0600

    Remove code duplication for server startup/cleanup.

--- startDevServer.sh
+++ startDevServer.sh
@@ -1,16 +1,10 @@
 #!/bin/bash/
 
 {
-python ./manage.py collectstatic -v0 --noinput
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 
-if [ $# -gt 0 ]
-then
-    if [ $1 == "clean" ]
-    then
-        python ./manage.py deletecache all
-    fi
-fi
+./cleanup.sh
 
-python ./manage.py purgeprocessqueue
 python ./manage.py runserver 5000
 }
