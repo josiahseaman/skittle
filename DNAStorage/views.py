71f368688244857f8831e04b517e7ab2bef9719c
Bryan <bryan@newlinetechnicalinnovations.com>
2013-05-24 17:09:53 -0600

    Speculative form pushing?

--- DNAStorage/views.py
+++ DNAStorage/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.utils import simplejson
 from DNAStorage.models import Specimen, FastaFiles
 from DNAStorage import StorageRequestHandler
+from StorageRequestHandler import HandleUploadedFile
+from django import forms
 
 
 def index(request):
@@ -12,10 +14,26 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def upload(request):
+class uploadFileForm(forms.Form):
+    availability = None  # TODO: Find out how to bring in the two radio buttons
+    kingdomName = forms.CharField()  # TODO: Change these char fields to fix what ever Marshall does for the taxonomy selection
+    className = forms.CharField()
+    genusName = forms.CharField()
+    speciesName = forms.CharField()
+    specimenName = forms.CharField()
+    genomeName = forms.CharField()
+    source = forms.CharField()
+    dateSequenced = forms.DateTimeField()
+    description = forms.CharField()
+    file = forms.FileField()
+
+def Upload(request):
     status = None
     message =""
     if request.is_ajax() or request.method == 'POST':
+        form = uploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            filePath = HandleUploadedFile(request.FILES['file'])
         # handle the form data?
         status = "success"
         message = "Your files were uploaded successfully. Please Allow a few hours for processing."
