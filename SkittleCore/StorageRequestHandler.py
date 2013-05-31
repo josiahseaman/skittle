7bf5c0c0f9aaa2d6293339da572ab3922c5c7c27
Bryan <bryan@newlinetechnicalinnovations.com>
2013-05-31 10:37:13 -0600

    Create methods for getting state and request from the user.

--- SkittleCore/StorageRequestHandler.py
+++ SkittleCore/StorageRequestHandler.py
@@ -2,7 +2,6 @@ from SkittleCore.models import RequestPacket, StatePacket, SkittleUser
 
 def SaveState(request, user):
     assert isinstance(request, RequestPacket)
-    assert isinstance(user, SkittleUser)
 
     statePacket = StatePacket.objects.filter(userId=user.id)[:1]
 
@@ -27,3 +26,35 @@ def SaveState(request, user):
 
     user.State = statePacket
     user.save()
+
+
+def GetUserState(user):
+    statePacket = StatePacket.objects.filter(userId=user.id)[:1]
+
+    if statePacket:
+        return statePacket[0]
+    else:
+        return None
+
+
+def GetUserRequestFromState(user):
+    statePacket = GetUserState(user)
+
+    if statePacket:
+        requestPacket = RequestPacket()
+
+        requestPacket.specimen = statePacket.specimen
+        requestPacket.chromosome = statePacket.chromosome
+        requestPacket.seq = statePacket.seq
+        requestPacket.colorPalette = statePacket.colorPalette
+        requestPacket.width = statePacket.width
+        requestPacket.scale = statePacket.scale
+        requestPacket.start = statePacket.start
+        requestPacket.length = statePacket.length
+        requestPacket.requestedGraph = statePacket.requestedGraph
+        requestPacket.searchStart = statePacket.searchStart
+        requestPacket.searchStop = statePacket.searchStop
+
+        return requestPacket
+    else:
+        return None
